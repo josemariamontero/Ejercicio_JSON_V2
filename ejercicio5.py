af4286a4d9f8a088e0cb17365d1a346b797374f1
Jose Maria Montero <josema9817@gmail.com>
2019-03-22 18:17:35 +0100

    Queda poner los numeros

--- ejercicio5.py
+++ ejercicio5.py
@@ -2,9 +2,22 @@ import json
 with open('movies.json','r') as fichero:
 	doc = json.load(fichero)
 
+indicador = False
+
+fecha = input("Dime una fecha de estreno: ")
+fecha2 = input("Dime otra fecha de estreno: ")
+
 for i in doc:
 	titulo = i["title"]
 	poster = i["posterurl"]
 	puntuacion = i["ratings"]
-	media = sum(puntuacion) / len(puntuacion)
-	print (titulo,media)
+	estreno = i["releaseDate"]
+	media = round(sum(puntuacion) / len(puntuacion))
+	if fecha <= estreno and fecha2 >= estreno:
+		indicador = True
+		print ("Titulo:",titulo)
+		print ("Media:",media)
+		print ("Poster:",poster)
+
+if not indicador:
+	print("No hay peliculas entre las fechas dadas")
